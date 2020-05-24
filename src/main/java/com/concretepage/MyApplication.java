94b8c045c2de00d74eb6e88c22205bde5b772f8f
jaggu <jaggu@localhost>
2020-05-23 17:39:55 -0700

    jnknn

--- src/main/java/com/concretepage/MyApplication.java
+++ src/main/java/com/concretepage/MyApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {  
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
-		
-		//jagadeesg chbages 
-    }       
+    
+	//chnages
+	
+	}       
 }            
